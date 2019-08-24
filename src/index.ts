6ca00ad2f75535db36d9eb0bb1985d62c25f836b
shift <qjfrntop12@naver.com>
2019-08-24 14:04:12 +0900

    Setup mongodb env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 import routes from './routes';
 
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello world!');
@@ -9,8 +14,14 @@ app.get('/', (req: express.Request, res: express.Response) => {
 
 app.use('/api', routes);
 
+require('mongoose').Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI as string, { useNewUrlParser: true })
+  .then(() => console.log('Server connected to mongodb'))
+  .catch((err: Error) => console.error(err));
+
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log(`Server started on port ${port}`);
 });
 
 export default app;
