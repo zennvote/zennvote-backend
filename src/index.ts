f1a8598c2098add07dc1c59dda505c71bf0ebc23
shift <qjfrntop12@naver.com>
2019-08-24 13:38:52 +0900

    Make api route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello world!');
 });
+
+app.use('/api', routes);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
--- test/app.test.ts
+++ test/app.test.ts
@@ -2,11 +2,13 @@ import { expect } from 'chai';
 import * as request from 'supertest';
 import index from '../src/index';
 
-describe('app.test', () => {
+describe('Route test', () => {
   const req = request(index);
 
-  it('GET /', async () => {
-    const res = await req.get('/').expect(200);
-    expect(res.text).to.equal('Hello world!');
+  it('GET /', () => {
+    return req
+      .get('/')
+      .expect(200)
+      .expect('Hello world!');
   });
 });
