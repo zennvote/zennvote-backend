d514cfec63ecff1d317dbee7a83677041daa2d49
shift <qjfrntop12@naver.com>
2019-08-28 04:15:42 +0900

    Fix voteDataSchema array type's notation

--- src/models/vote.ts
+++ src/models/vote.ts
@@ -1,11 +1,11 @@
 import * as mongoose from 'mongoose';
 
-interface Vote extends mongoose.Document {
+export interface Vote extends mongoose.Document {
   data: VoteData;
   email: string;
 }
 
-interface VoteData extends mongoose.Document{
+export interface VoteData extends mongoose.Document{
   problem: number[];
   pitch: string[];
   voice: string[];
@@ -17,10 +17,10 @@ interface VoteData extends mongoose.Document{
   new: string;
   grow: string;
   master: string[];
-  custom: [{
+  custom: {
     episode: string;
     content: string;
-  }],
+  }[];
   message: {
     name: string;
     content: string;
@@ -29,16 +29,16 @@ interface VoteData extends mongoose.Document{
 
 const voteDataSchema = new mongoose.Schema({
   problem: [Number],
-  pitch: [{ type: String, required: true }],
-  voice: [{ type: String, required: true }],
-  funny: [{ type: String, required: true }],
-  content: [{ type: String, required: true }],
-  original: [{ type: String, required: true }],
+  pitch: { type: [String], required: true },
+  voice: { type: [String], required: true },
+  funny: { type: [String], required: true },
+  content: { type: [String], required: true },
+  original: { type: [String], required: true },
   sleep: { type: String, required: true },
   unit: { type: String, required: true },
   new: { type: String, required: true },
   grow: { type: String, required: true },
-  master: [{ type: String, required: true }],
+  master: { type: [String], required: true },
   custom: [new mongoose.Schema({
     episode: { type: String, required: true },
     content: { type: String, required: true },
