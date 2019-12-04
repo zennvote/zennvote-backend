1fd7a476f8f39d31067a2cbb6e5869f654a38fe9
shift <qjfrntop12@naver.com>
2019-12-04 21:59:26 +0900

    lint 적용

--- src/routes/episodes.ctrl.ts
+++ src/routes/episodes.ctrl.ts
@@ -4,25 +4,25 @@ import { isUndefined } from 'util';
 import { getEpisodeData, getEpisodesWithProducer } from '../utils/sheet';
 
 export const getEpisodeWithNumber = async (req: Request, res: Response) => {
-    const { episode, index } = req.query;
-    const episodeData = await getEpisodeData(Number(episode), Number(index));
+  const { episode, index } = req.query;
+  const episodeData = await getEpisodeData(Number(episode), Number(index));
 
-    if (isUndefined(episodeData)) {
-        res.status(404).json({ episode, index, message: 'no episode' });
-        return;
-    }
+  if (isUndefined(episodeData)) {
+    res.status(404).json({ episode, index, message: 'no episode' });
+    return;
+  }
 
-    res.json({ ...episodeData, episode, index });
+  res.json({ ...episodeData, episode, index });
 };
 
 export const getEpisodeWithProducer = async (req: Request, res: Response) => {
-    const { producer } = req.query;
-    const episodeData = await getEpisodesWithProducer(producer);
+  const { producer } = req.query;
+  const episodeData = await getEpisodesWithProducer(producer);
 
-    if (isUndefined(episodeData)) {
-        res.status(404).json({ producer, message: 'no producer' });
-        return;
-    }
+  if (isUndefined(episodeData)) {
+    res.status(404).json({ producer, message: 'no producer' });
+    return;
+  }
 
-    res.json({ episodes: episodeData, producer });
+  res.json({ producer, episodes: episodeData });
 };
